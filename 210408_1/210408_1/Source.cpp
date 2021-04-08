862f1c2ad168d871d47795247eac59ccda22b073
saee0123 <saee0123@knou.ac.kr>
2021-04-08 21:10:47 +0900

    연습문제1

---
+++ 210408_1/210408_1/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+void main() {
+	for (int _inumber0 = 0; _inumber0 < 5; _inumber0++) {
+		for (int _inumber1 = 0; _inumber1 <= _inumber0; _inumber1++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+fgetc(stdin);
+}
